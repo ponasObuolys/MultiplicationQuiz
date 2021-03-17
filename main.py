83b5f1f442b30b24e3b3a977e4cfd91c662f44c9
AurisBut <a.butvilauskas@gmail.com>
2021-03-17 10:15:02 +0200

    repeated loop

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 import random
 from colorama import *
 
-bandymai = int(input(Fore.YELLOW +'\nKelis kartus nori spėti?- '))
-print(Style.RESET_ALL)
-spejimai = 0
-atsakyti = []
 
-while True:
 
-    x = random.randint(2, 9)
-    y = random.randint(2, 9)
-    teisingas = x * y
-    print('|------------------------------------------|')
-    print('\tKiek bus ' + str(x) + ' padauginus is ' + str(y) + '?')
-    atsakimas = int(input(Fore.BLUE + '\tĮrašyk atsakymą ir paspausk ENTER:- '))
-    print('|------------------------------------------|')
+def matematika():
+    bandymai = int(input(Fore.YELLOW + '\nKelis kartus nori spėti?- '))
+    print(Style.RESET_ALL)
+    spejimai = 0
+    atsakyti = []
 
-    if atsakimas == teisingas:
-        bandymai -= 1
-        spejimai += 1
-        atsakyti.append([f'{x} x {y} = {teisingas}'])
-        print(Fore.GREEN + '\tŠaunuolė! Atsakei i {} klausymą(-us)'.format(spejimai))
-        print(f'\tLiko {bandymai} klausimas(-ai)')
-        print(Style.RESET_ALL)
-        if bandymai == 0:
-            stringas = ' '.join([str(elem) for elem in atsakyti])
-            print(Fore.MAGENTA + 'Atsakei teisingai {} kartus(-ų) iš eilės.'.format(spejimai))
-            print(Fore.YELLOW + 'Teisingi atsakymai buvo šie: {}'.format(stringas))
-            break
+    while True:
+
+        x = random.randint(2, 9)
+        y = random.randint(2, 9)
+        teisingas = x * y
+        print('|------------------------------------------|')
+        print('\tKiek bus ' + str(x) + ' padauginus is ' + str(y) + '?')
+        atsakimas = int(input(Fore.BLUE + '\tĮrašyk atsakymą ir paspausk ENTER:- '))
+        print('|------------------------------------------|')
+
+        if atsakimas == teisingas:
+            bandymai -= 1
+            spejimai += 1
+            atsakyti.append([f'{x} x {y} = {teisingas}'])
+            print(Fore.GREEN + '\tŠaunuolė! Atsakei i {} klausymus(-ą)'.format(spejimai))
+            print(f'\tLiko {bandymai} klausimas(-ai)')
+            print(Style.RESET_ALL)
+            if bandymai == 0:
+                print(Fore.MAGENTA + 'Atsakei teisingai {} kartų(-us) iš eilės.'.format(spejimai))
+                print(Fore.YELLOW + 'Teisingi atsakymai buvo šie:')
+                print(*atsakyti, sep="\n")
+                break
+            else:
+                pass
         else:
-            pass
-    else:
-        print(Fore.RED + '\nNETEISINGAI!. Teisingas atsakymas: ' + Fore.GREEN + '{}'.format(teisingas))
-        print(Fore.RED + 'Atsakei teisingai tik {} kartą(-us) iš eilės.'.format(spejimai))
-        break
+            print(Fore.RED + '\nNETEISINGAI!. Teisingas atsakymas: ' + Fore.GREEN + '{}'.format(teisingas))
+            print(Fore.RED + 'Atsakei teisingai tik {} kartą(-us) iš eilės.'.format(spejimai))
+            print(Fore.YELLOW + 'Teisingi atsakymai buvo šie:')
+            print(*atsakyti, sep="\n")
+            print(Style.RESET_ALL)
+            kartojimas = input(Fore.GREEN + 'Nori bandyti dar kartą? (T/N)- ').lower()
+            if kartojimas in ['t', 'taip']:
+                matematika()
+            else:
+                break
+
+matematika()
